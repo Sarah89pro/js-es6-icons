f93aefec0d4d4858dc8fdf707195d1373469f688
Sarah89Pro <sarahproietti1@gmail.com>
2021-04-19 17:35:31 +0200

    .

--- js/script.js
+++ js/script.js
@@ -102,3 +102,43 @@ const icons = [
     },
 ];
 
+//referenza contenitore
+//Icon Container
+const container = document.querySelector(".icons"); //prende il selettore
+
+//1 Stampare le icone a schermo
+printIcons(icons, container); //dove mettere le icone e le icone stesse (sono i due parametri per la funzione)
+
+
+
+
+
+
+
+/***********
+ * UTILITY *
+ ***********/
+
+//Funzione per stampare le icone a schermo
+function printIcons(icons, container) { //non collidono! Qui sono parametri
+
+    //markup icone
+    let html =" "; //uso let perchè va costruita, verrà sovrascritta ogni volta
+    //loop
+    icons.forEach(element => { //icon qui sta comunque per element!
+
+        //destrutturazione, tante variabili in un colpo solo
+        const {name, prefix, family} = element;//l'oggetto da cui vogliamo estrarre in ogni iterazione
+
+        html += `
+        <div class="icon p-20">
+            <i class= "${family} ${prefix} ${name}" style="color: #333"></i>
+            <div class="title">${name}</div>
+        </div>
+        `;               //+= per aggiungere un pezzo di markup ad ogni loop
+                        // interpolazioni: family -> fas   prefix -> fa-   name -> cat parametri dinamici
+    });
+
+    //inserimento icone nel Container
+    container.innerHTML = html; //dopo averlo costruito lo inserisco nell'html, assegnazione
+}
